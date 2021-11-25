c6740d9287a716cf7e4f809ea27ef4c75d258e27
heyooh <360678527@qq.com>
2021-11-26 03:53:58 +0800

    项目初始结构创建

--- gulimall-order/src/main/java/com/heyooh/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/heyooh/gulimall/order/GulimallOrderApplication.java
@@ -1,8 +1,10 @@
 package com.heyooh.gulimall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.heyooh.gulimall.order.dao")
 @SpringBootApplication
 public class GulimallOrderApplication {
 
