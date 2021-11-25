2b1f2ff933fc3fdd555d9037a0db3fe0284832bd
heyooh <360678527@qq.com>
2021-11-25 17:48:08 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/heyooh/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyooh.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
